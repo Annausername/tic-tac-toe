c2a6bbcbcce92d681f372d0a835354929aaf3d41
Anna <annklimova10@gmail.com>
2022-11-08 00:11:11 +0000

    Innitial commit with import modules

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import time  # time delay for game intro
+import os # for the underlying os interaction
+import sys # for built-in modules
