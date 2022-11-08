ac6faebe441404e9bac9219cbb13177b5b9b857b
Anna <annklimova10@gmail.com>
2022-11-08 00:12:46 +0000

    Printing out the welcome title and get the player name

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 import time  # time delay for game intro
 import os # for the underlying os interaction
 import sys # for built-in modules
+
+print("Welcome to the classic Tic-Tac-Toe game (also called Noughts and Crosses) ^‿◕\n")
+def player_name():
+    """
+    Gets a player name and prints out a welcome message.
+    """
+    while True:
+        name = input("\nWhat is your name, stranger?: \n").capitalize()
+        print(f"\nLet's roll, {name}!")
+        break
