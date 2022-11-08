e15ab0f01391934bd003aa26412e06eca60fb240
Anna <annklimova10@gmail.com>
2022-11-08 00:14:50 +0000

    Add time delay fuction to print text in a typewriter style

--- run.py
+++ run.py
@@ -2,7 +2,17 @@ import time  # time delay for game intro
 import os # for the underlying os interaction
 import sys # for built-in modules
 
-print("Welcome to the classic Tic-Tac-Toe game (also called Noughts and Crosses) ^‿◕\n")
+def message(text):
+    """
+    Prints out a message with a time delay.
+    """
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(.1)
+    time.sleep(1)
+
+message("Welcome to the classic Tic-Tac-Toe game (also called Noughts and Crosses) ^‿◕\n")
 def player_name():
     """
     Gets a player name and prints out a welcome message.
@@ -11,3 +21,4 @@ def player_name():
         name = input("\nWhat is your name, stranger?: \n").capitalize()
         print(f"\nLet's roll, {name}!")
         break
+player_name()
